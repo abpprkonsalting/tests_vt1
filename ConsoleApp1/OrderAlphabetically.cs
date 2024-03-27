2c71d234e0b3ac7a4a321eef30e3e466de9b9f56
abpprkonsalting <abpprkonsalting@gmail.com>
2024-03-27 18:34:22 -0400

    Add project files.

---
+++ ConsoleApp1/OrderAlphabetically.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testsVT
+{
+    internal class OrderAlphabetically
+    {
+        private string phrase { get; set; } = "";
+        private string ordered { get; set; }
+
+        public OrderAlphabetically(string phrase)
+        {
+            if (!string.IsNullOrEmpty(phrase))
+            {
+                throw new Exception("La frase no puede ser vacia");
+            }
+            this.phrase = phrase;
+        }
+        public string order()
+        {
+            this.ordered = phrase;
+            return ordered;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testsVT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 1)
+            {
+                Console.Write("Uso del programa: test_vt <# del test>\n");
+                Console.Write("Donde # del test puede ser:\n");
+                Console.Write("\t1- Ordenación alfabética de frase \n");
+                Console.Write("\t2- Cálculo de salario \n");
+                return;
+            }
+            try
+            {
+                switch(args[0])
+                {
+                    case "1":
+                        Console.Write("Entre la frase a ordenar alfabeticamente:");
+                        var phrase = Console.ReadLine();
+                        var order = new OrderAlphabetically(phrase);
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        throw new Exception("Valor de entrada incorrecto");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write("Exception:");
+                Console.Write(e.Message);
+            }
+
+        }
+    }
+}
